f5bcdf465e13f6baf88c5ec4c1833a636d9db129
IgorPolst <igorpolstianoy@gmail.com>
2023-11-18 13:35:50 +0300

    Добавил файлы

---
+++ Repl.it/C++/library_math.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+  float a, S;
+  cin >> a; 
+  S = (sqrt(3)*pow(a,2))/4;
+  cout << S;
+}
---
+++ Repl.it/Python/library_math.py
@@ -0,0 +1,5 @@
+import math
+
+a = int(input())
+c = (math.sqrt(3) * a**2) / 4
+print(c)
