f489905c187a612b96efe00257cd07eda01cb24c
IgorPolst <igorpolstianoy@gmail.com>
2023-11-24 16:55:07 +0300

    Close 4.1

--- Stepic/4.1/step_10.py
+++ Stepic/4.1/step_10.py
@@ -5,3 +5,14 @@
 # Программа должна вывести наименьшее из четырёх чисел.
 
 
+first = int(input())
+second = int(input())
+third = int(input())
+forsth = int(input())
+if first > second:
+    first = second
+if first > third:
+    first = third
+if first > forsth:
+    first = forsth
+print(first)
