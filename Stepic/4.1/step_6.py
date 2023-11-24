2e1a4b849c299de1ec869a34e25e4f989136e338
IgorPolst <igorpolstianoy@gmail.com>
2023-11-24 15:36:02 +0300

    work at 24.11

--- Stepic/4.1/step_6.py
+++ Stepic/4.1/step_6.py
@@ -5,5 +5,9 @@
 # Программа должна вывести «ДА», если соотношение выполняется, и «НЕТ» — если не выполняется.
 
 number = int(input())
-first = sum(number%10, number//1000)
-second = 
+first = number % 10 + number // 1000
+second = number % 1000 // 100 - number % 100 // 10
+if first == second:
+    print("ДА")
+else:
+    print("НЕТ")
