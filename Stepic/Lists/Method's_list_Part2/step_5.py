9b894df755f4757215784fc8470e6badf6c4ab64
IgorPolst <igorpolstianoy@gmail.com>
2024-01-06 02:59:44 +0300

    Finish 11.6

--- Stepic/Lists/Method's_list_Part2/step_5.py
+++ Stepic/Lists/Method's_list_Part2/step_5.py
@@ -1,9 +1,7 @@
 numbers = [8, 9, 10, 11]
 numbers[1] = 17
-numbers.append(4)
-numbers.append(5)
-numbers.append(6)
-numbers.remove(numbers[0])
-numbers.append(numbers.copy())
-numbers.insert(2,25)
+numbers.extend([4, 5, 6])
+del numbers[0]
+numbers *= 2
+numbers.insert(3, 25)
 print(numbers)
