550ed769f3181438265fa17199083cf399dc81b7
IgorPolst <igorpolstianoy@gmail.com>
2024-01-21 20:54:46 +0300

    Finish return function part3

--- Stepic/Function/Function_return_part_2/step_10.py
+++ Stepic/Function/Function_return_part_2/step_10.py
@@ -17,3 +17,21 @@ txt = input()
 
 # вызываем функцию
 print(convert_to_python_case(txt))
+
+# # объявление функции
+# def convert_to_python_case(text):
+#     new_text = ""
+#     for el in text:
+#         if not el == el.lower():  # проверяем, что элемент в верхнем регистре (пропускаем цифры)
+#             new_text += "_" + el.lower()
+#         else:
+#             new_text += el
+            
+#     return new_text[1:]
+
+
+# # считываем данные
+# txt = input()
+
+# # вызываем функцию
+# print(convert_to_python_case(txt))
