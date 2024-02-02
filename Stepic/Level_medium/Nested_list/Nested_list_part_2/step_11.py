cbd30b2205a7edf11a2029deaf80e1d942ea68d5
IgorPolst <igorpolstianoy@gmail.com>
2024-02-02 15:25:22 +0300

    Finish_Nested_list

--- Stepic/Level_medium/Nested_list/Nested_list_part_2/step_11.py
+++ Stepic/Level_medium/Nested_list/Nested_list_part_2/step_11.py
@@ -1,14 +1,16 @@
 n = int(input())
-list1 = [[1], [1, 1]]
-if n == 0:
-    print(list1[0])
-elif n == 1:
-    print(list1[1])
-else:
+
+
+def pascal(n):
+    list1 = [[1], [1, 1]]
     for i in range(1, n):
         new_list = [1]
         for j in range(len(list1[i]) - 1):
             new_list.append(list1[i][j] + list1[i][j + 1])
         new_list.append(1)
         list1.append(new_list)
-    print(list1[n])
+    return list1[n]
+
+
+for i in range(n):
+    print(*pascal(i))
