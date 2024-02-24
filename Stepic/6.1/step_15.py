fed549b695b0b71748e603c09b8f7212ae9a923a
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2024-02-25 01:52:21 +0300

    Delete Stepic/6.1 directory

--- Stepic/6.1/step_15.py
+++
@@ -1,9 +0,0 @@
-a1, a2, a3, a4, a5 = (
-    float(input()),
-    float(input()),
-    float(input()),
-    float(input()),
-    float(input())
-)
-
-print(abs(a1) + abs(a2) + abs(a3) + abs(a4) + abs(a5))
--- Stepic/6.1/step_7.py
+++
@@ -1,6 +0,0 @@
-age = float(input())
-
-if 1 <= age <= 2:
-    print(age * 10.5)
-else:
-    print(21 + (age - 2) * 4)
