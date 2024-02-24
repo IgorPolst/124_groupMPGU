252c7755a0766d50b512ac333fbd6a1caa934b11
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2024-02-25 01:52:26 +0300

    Delete Stepic/6.2 directory

--- Stepic/6.2/step_14.py
+++
@@ -1,6 +0,0 @@
-string = str(input())
-
-if "@" in string and "." in string:
-    print("YES")
-else:
-    print("NO")
--- Stepic/6.2/step_6.py
+++
@@ -1,3 +0,0 @@
-name, lastname = str(input()), str(input())
-
-print("Hello " + name + " " + lastname + "! You have just delved into Python")
