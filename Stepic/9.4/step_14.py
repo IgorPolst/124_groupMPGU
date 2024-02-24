ec442d815ef81ce8291dafd2a493feb06c6ac172
IgorPolst <71287351+IgorPolst@users.noreply.github.com>
2024-02-25 01:53:58 +0300

    Delete Stepic/9.4 directory

--- Stepic/9.4/step_14.py
+++
@@ -1,8 +0,0 @@
-s = str(input())
-
-if s.count("f") == 1:
-    print(s.find("f"))
-elif s.count("f") > 1:
-    print(s.find("f"), s.rfind("f"))
-else:
-    print("NO")
--- Stepic/9.4/step_8.py
+++
@@ -1,8 +0,0 @@
-s = str(input())
-total = 1
-
-for i in range(len(s)):
-    if s[i] == " ":
-        total += 1
-
-print(total)
