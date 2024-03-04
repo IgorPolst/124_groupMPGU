9c3740e2f0d3bc7d893b1e5b67e7b18e41b49450
IgorPolst <igorpolstianoy@gmail.com>
2024-03-04 23:34:35 +0300

    Закончил задачки на словари

--- Stepic/Level_medium/Dictionary/Exercise_dictionary/step_4.py
+++ Stepic/Level_medium/Dictionary/Exercise_dictionary/step_4.py
@@ -4,10 +4,11 @@ for i in range(int(input())):
     simbl = text.index(" ")
     dict2.setdefault(text[:simbl], text[simbl + 1 :])
 
-
 quest = input()
 
 if quest in dict2:
     print(dict2[quest])
 else:
-    v
+    for k, v in dict2.items():
+        if v == quest:
+            print(k)
